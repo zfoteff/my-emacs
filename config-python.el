69eed8dfcd5096d88a383fbd3f1cc1dcc5a48517
zfoteff <zfoteff@zagmail.gonzaga.edu>
2023-12-03 15:13:54 -0800

    python config update

--- config-python.el
+++ config-python.el
@@ -3,9 +3,14 @@
 ;;; Commentary: Python configuration for my Emacs
 
 ;; Python Configuration
-(require 'lsp-python-ms)
-(setq lsp-python-ms-auto-install-server t)
+(setq lsp-pylsp-plugins-flake8-max-line-length 88)
 (add-hook 'python-mode-hook #'lsp) ; or deferred-lsp
 
 (provide 'config-python)
 ;;; config-python.el ends here
+
+(use-package! python-black
+  :demand t
+  :after python
+  :config
+  (add-hook! 'python-mode-hook #'python-black-on-save-mode))
